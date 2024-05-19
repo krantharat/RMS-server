29af5f2e82cc56e1443166eaa0f20e27f8b4dcda
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-05-19 22:42:13 +0700

    add function in stock.controller

--- src/Models/stock.model.js
+++ src/Models/stock.model.js
@@ -10,3 +10,49 @@ const Uom = new mongoose.Schema({
 
 const UomModel = mongoose.model('Uom',Uom) 
 module.exports = UomModel;
+
+// Ingredient Category Schema
+const IngredientCategory = new mongoose.Schema({
+    ingredientCategoryName: {
+        type: String,
+        required: true
+    },
+});
+
+const IngredientCategoryModel = mongoose.model('IngredientCategory',IngredientCategory) 
+module.exports = IngredientCategoryModel;
+
+// Ingredient Schema
+const Ingredient = new mongoose.Schema({
+    ingredientName: {
+        type: String,
+        required: true
+    },
+    ingredientCategory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"IngredientCategory",
+        required: true
+    },
+    date: {
+        type: String,
+        require: false,
+        default: Date.now
+    },
+    inStock: {
+        type: Number,
+        require: false,
+        default: 0
+    },
+    uomType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Uom",
+        required: true
+    },
+    cost: {
+        type: Number,
+        require: true,
+    }
+});
+
+const IngredientModel = mongoose.model('Ingredient',Ingredient) 
+module.exports = IngredientModel;
--- src/Routers/stock.router.js
+++ src/Routers/stock.router.js
@@ -11,4 +11,20 @@ router.get("/", (req, res) => {
 
 router.post("/createUom", stockController.createUom);
 
+router.post("/createIngredientCategory", stockController.createIngredientCategory);
+
+router.post("/createIngredient", stockController.createIngredient);
+
+router.get("/allIngredient", stockController.allIngredient);
+
+router.get("/searchIngredient", stockController.searchIngredient);
+
+router.get("/searchIngredientByDate", stockController.searchIngredientByDate);
+
+router.put("/editIngredient/:id", stockController.editIngredient);
+
+router.delete('/deleteIngredient/:id', stockController.deleteIngredient);
+
+router.put("/updateIngredient/:id", stockController.updateIngredient);
+
 module.exports = router;
